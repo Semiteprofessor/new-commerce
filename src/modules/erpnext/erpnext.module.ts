715e44a0baf7196942275ad180b84ef653f73163
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-22 00:05:30 +0100

    Commit

--- src/modules/erpnext/erpnext.module.ts
+++ src/modules/erpnext/erpnext.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ErpNextService } from './services/erpnext.service';
+import { OrderModule } from '../apps/shop/order/order.module';
 
 @Module({
   imports: [ProductModule, OrderModule],

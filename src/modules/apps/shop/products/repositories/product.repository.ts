fab6ae4737755b0f990a34afa112f62518c182b5
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 22:39:52 +0100

    Commit

--- src/modules/apps/shop/products/repositories/product.repository.ts
+++ src/modules/apps/shop/products/repositories/product.repository.ts
@@ -17,4 +17,44 @@ export class ProductRepository extends EntityRepository<ProductEntity> {
     super(_productRepository);
     this._categoryRepository = this.entityManager.getTreeRepository(Category);
   }
+
+  async getProductsForTimeline(categorySlug: string): Promise<any> {
+    const parentCategory = await this._categoryRepository.findOne({
+      where: { slug: categorySlug },
+    });
+    if (!parentCategory) {
+      throw new Error(`Category with slug "${categorySlug}" not found`);
+    }
+
+    const descendants =
+      await this._categoryRepository.findDescendants(parentCategory);
+    const allCategoryIds = descendants.map((cat) => cat.id);
+
+    const wishlistProducts = await this.wishlistRepository.find(
+      {},
+      { relations: ['products'] },
+    );
+    const wishlistProductIds = wishlistProducts
+      .map((wishlist) => wishlist.products.map((p) => p.id))
+      .flat();
+
+    const productQuery = this._productRepository
+      .createQueryBuilder('products')
+      .innerJoin(
+        'categories',
+        'categories',
+        'products.categoryId = categories.id',
+      )
+      .where('products.deletedAt IS NULL')
+      .andWhere('products.categoryId IN (:...categoryIds)', {
+        categoryIds: allCategoryIds,
+      })
+      .select([
+        'products',
+        'categories.name AS categoryName',
+        'categories.slug AS categorySlug',
+      ]);
+
+    const rawProducts = await productQuery.getRawMany();
+  }
 }

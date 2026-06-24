0934fc5e21a836152e98fd3715d541d2fe22c6ed
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 18:04:49 +0100

    Commit

--- src/modules/common/interceptors/app.interceptor.ts
+++ src/modules/common/interceptors/app.interceptor.ts
@@ -1,10 +1,20 @@
-import { Inject, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Inject, NestInterceptor } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Observable } from 'rxjs';
 
 function dataIsPaginated(data: any): boolean {
   return !!data.data;
 }
 
 export class AppInterceptor implements NestInterceptor {
-    constructor(@Inject(WINSTON_MODULE_PROVIDER)) {}
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: any) {}
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+      const req: Request = context.switchToHttp().getRequest();
+      const { ip, method, originalUrl, body } = req;
+      const userAgent = req.get('user-agent') || '';
+
+      const className = context.getClass().name;
+      const handler = context.getHandler().name;
+      const statusCode = context.switchToHttp().getResponse().statusCode;
+  }
 }

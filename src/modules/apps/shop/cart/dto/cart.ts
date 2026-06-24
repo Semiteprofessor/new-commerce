b8a3ae773713bffa257af3d2229c43261ef59c3d
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 23:17:17 +0100

    Commit

--- src/modules/apps/shop/cart/dto/cart.ts
+++ src/modules/apps/shop/cart/dto/cart.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
 
 export class AddToCartDto {
   @ApiProperty()
@@ -49,3 +49,35 @@ export class CheckoutDto {
   paymentMethod: string;
 }
 
+export class BuyNowDto {
+  @ApiProperty({ example: '08cbaa9b-651c-4e27-964b-4945e55ec133' })
+  @IsString()
+  @IsNotEmpty()
+  shippingAddressId: string;
+
+  @ApiProperty({ example: '08cbaa9b-651c-4e27-964b-4945e55ec133' })
+  @IsString()
+  @IsNotEmpty()
+  productId: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+
+  @ApiProperty({ example: 'card' })
+  @IsString()
+  @IsNotEmpty()
+  paymentMethod: string;
+}
+
+export class UpdateCartItemDto {
+  @ApiProperty()
+  @IsString()
+  productId: string;
+
+  @ApiProperty()
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}

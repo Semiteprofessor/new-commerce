9d7faa719e5b634b490d244c8ced0258be396a60
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 23:28:39 +0100

    Commit

--- src/modules/erpnext/services/erpnext.service.ts
+++ src/modules/erpnext/services/erpnext.service.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { ProductService } from 'src/modules/apps/shop/products/services/product.service';
 import axios from 'axios';
 import { User } from 'src/modules/core/users/entities/user.entity';
+import { BusinessProfile } from 'src/modules/apps/shop/merchants/entities/business-profile.entity';
 @Injectable()
 export class ErpNextService {
   private readonly apiClient;
@@ -37,6 +38,37 @@ export class ErpNextService {
       );
 
       console.log(res.data.message.body);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  async createMerchant(data: User, business: BusinessProfile) {
+    try {
+      const res = await this.apiClient.post(
+        'ecommerce.controllers.merchant_controller.add_merchant',
+        {
+          merchant_id: data.id,
+          first_name: data.firstName,
+          last_name: data.lastName,
+          username: data.username,
+          email: data.email,
+          password: data.password,
+          phone: data.phone,
+          role: data.role,
+          business_name: business.name,
+          business_type: business.businessType,
+          business_category: business.businessCategory,
+          business_address: data.address,
+          business_location_lat: business.businessLocationLat,
+          business_location_long: business.businessLocationLong,
+          street: 'Spencer',
+          city: 'Ikeja',
+          state: 'Lagos',
+          country: 'Nigeria',
+        },
+      );
     } catch (e) {
       console.log(e);
       throw e;

364bd21d366d9a82cf4ed5d70efc4a5584d0877e
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 21:09:52 +0100

    Commit

--- src/modules/core/users/controllers/user.controller.ts
+++ src/modules/core/users/controllers/user.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../services/user.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ActiveUser } from '../../iam/decorators/active-user.decorator';
+import { SubscribeToNewsletterDto, UpdateUserInfoDto } from '../../iam/authentication/dtos/auth.dto';
 
 @Controller('v1')
 export class UserController {
@@ -22,4 +25,34 @@ export class UserController {
   //   }
   //   return this.userService.update(userId, data);
   // }
+
+  @Patch('users/update')
+  @ApiOperation({ summary: 'Update user information' })
+  @ApiResponse({
+    status: 200,
+    description: 'User information updated successfully.',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request data.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @ApiResponse({ status: 409, description: 'Email already in use.' })
+  async updateUser(
+    @ActiveUser() user: ActorUser,
+    @Body() updateUserDto: UpdateUserInfoDto,
+  ) {
+    console.log('ActiveUser:', user);
+    if (!user || !user.id) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+    return this.userService.updateUser(user.id, updateUserDto);
+  }
+
+  @ApiBearerAuth()
+  @Patch('users/newsletter/subscribe')
+  @ApiOperation({ summary: 'subscribe to newsletter' })
+  async subscribeOrUnsubscribeToNewsletter(
+    @ActiveUser() user: ActorUser,
+    @Body() data: SubscribeToNewsletterDto,
+  ) {
+    return this.userService.subscribeOrUnsubscribeToNewsletter(user.id, data);
+  }
 }

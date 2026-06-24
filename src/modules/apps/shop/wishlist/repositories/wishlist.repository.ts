1e93f2f643d97312dc09f0c81c65e9741f6617c9
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 22:54:04 +0100

    Commit

--- src/modules/apps/shop/wishlist/repositories/wishlist.repository.ts
+++ src/modules/apps/shop/wishlist/repositories/wishlist.repository.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Wishlist } from "../entities/wishlist.entity";
 import { EntityRepository } from "src/db/repository/entity.repository";
+import { PageInfo, PaginatedRecordsDto, QueryParamsDto } from "src/modules/common/dtos/pagination.dto";
+import { Product } from "../../products/entities/product.entity";
 
 @Injectable()
 export class WishlistRepository extends EntityRepository<Wishlist> {
@@ -11,5 +13,46 @@ export class WishlistRepository extends EntityRepository<Wishlist> {
     private readonly _wishlistRepository: Repository<Wishlist>,
   ) {
     super(_wishlistRepository);
+  }
+
+  async findAllWishlistByQueryBuilder(
+    query: QueryParamsDto,
+  ): Promise<PaginatedRecordsDto<Product>> {
+    const {
+      userId,
+      sortBy = 'createdAt',
+      sortOrder = 'DESC',
+      limit = 10,
+      page = 1,
+    } = query;
+
+    const wishlistQuery = this._wishlistRepository
+      .createQueryBuilder('wishlist')
+      .leftJoinAndSelect('wishlist.products', 'product')
+      .leftJoinAndSelect('product.business', 'business')
+      .where('wishlist.deletedAt IS NULL');
+
+    if (userId) {
+      wishlistQuery.andWhere('wishlist.userId = :userId', { userId });
+    }
+
+    const totalCount = await wishlistQuery.clone().getCount();
+
+    const wishlist = await wishlistQuery
+      .orderBy(`wishlist.${sortBy}`, sortOrder as 'ASC' | 'DESC')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getMany();
+
+    const products = wishlist.flatMap((item) => item.products);
+
+    const pageInfo: PageInfo = {
+      total: totalCount,
+      page,
+      limit,
+      totalPages: Math.ceil(totalCount / limit),
+    };
+
+    return { data: products, pageInfo };
   }
 }

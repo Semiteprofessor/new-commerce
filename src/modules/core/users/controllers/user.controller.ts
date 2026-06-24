9338b361ccf6d1586edbd2a8c25fc986bcafc271
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-24 20:58:02 +0100

    Commit

--- src/modules/core/users/controllers/user.controller.ts
+++ src/modules/core/users/controllers/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 
 @Controller('v1')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('users/me')
+  async me(@ActiveUser('id') authUserId: any): Promise<Partial<User>> {
+    return this.userService.me(authUserId);
+  }
+  //
+  // @Patch('users/:id')
+  // async update(
+  //   @ActiveUser() authUserData: User,
+  //   @Param('id') userId: string,
+  //   @Body() data: UpdateUserDto,
+  // ) {
+  //   const { id: _id, role } = authUserData;
+  //   if (_id !== userId) {
+  //     throw new BadRequestException('Profile not found');
+  //   }
+  //   return this.userService.update(userId, data);
+  // }
 }

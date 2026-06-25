29a22de1ae9156c8bf91ee2d970d964fd137bc14
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-25 12:52:59 +0100

    Commit

--- src/db/repository/entity.repository.ts
+++ src/db/repository/entity.repository.ts
@@ -1,5 +1,24 @@
-import { Repository } from 'typeorm';
+import {
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 
 export abstract class EntityRepository<T> {
   constructor(protected readonly entityRepository: Repository<T>) {}
+
+  async find(
+    filterQuery: FindOptionsWhere<T>,
+    options?: FindManyOptions<T>,
+  ): Promise<T[] | null> {
+    return this.entityRepository.find({ where: filterQuery, ...options });
+  }
+
+  async findOne(
+    filterQuery: FindOptionsWhere<T>,
+    options?: FindOneOptions<T>,
+  ): Promise<T | null> {
+    return this.entityRepository.findOne({ where: filterQuery, ...options });
+  }
 }

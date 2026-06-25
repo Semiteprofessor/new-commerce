fedf5e5043939e6a7062ac6fa8fa1567860eb486
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-25 17:02:16 +0100

    Commit

--- src/modules/common/dtos/pagiination.dto.ts
+++ src/modules/common/dtos/pagiination.dto.ts
@@ -1,4 +1,5 @@
-import { IsOptional, IsIn } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsOptional, IsIn, IsString, IsDateString } from 'class-validator';
 
 enum SortOrder {
   ASC = 'ASC',
@@ -39,4 +40,39 @@ export class QueryParamsDto {
 
   @IsOptional()
   userId?: string;
+
+  @IsDateString()
+  @IsOptional()
+  startsAt?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  endsAt?: Date;
+
+  @IsOptional()
+  shortage?: number;
+
+  @IsOptional()
+  @Transform(({ value }) =>
+    isNaN(parseInt(value, 10)) ? 25 : parseInt(value, 10),
+  )
+  limit = 25;
+
+  @IsOptional()
+  @Transform(({ value }) =>
+    isNaN(parseInt(value, 10)) ? 1 : parseInt(value, 10),
+  )
+  page = 1;
+
+  @IsString()
+  @IsOptional()
+  status: string;
+
+  @IsString()
+  @IsOptional()
+  sender: string;
+
+  @IsString()
+  @IsOptional()
+  courier: string;
 }

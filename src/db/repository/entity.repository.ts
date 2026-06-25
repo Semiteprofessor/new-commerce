263657275fd6ff45ed5a11c5029a4db3144aa2fe
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-25 15:11:24 +0100

    Commit

--- src/db/repository/entity.repository.ts
+++ src/db/repository/entity.repository.ts
@@ -1,5 +1,5 @@
 import {
-    DeepPartial,
+  DeepPartial,
   FindManyOptions,
   FindOneOptions,
   FindOptionsWhere,
--- src/modules/core/users/repositories/user.repository.ts
+++ src/modules/core/users/repositories/user.repository.ts
@@ -1,5 +1,31 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "../entities/user.entity";
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { EntityRepository } from 'src/db/repository/entity.repository';
+import { DataSource, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserRepository extends EntityRepository<User>
+export class UserRepository extends EntityRepository<User> {
+  constructor(
+    @InjectRepository(User)
+    private readonly _userRepository: Repository<User>,
+    private readonly dataSource: DataSource,
+  ) {
+    super(_userRepository);
+  }
+
+  async create(data: Partial<User>): Promise<User> {
+    try {
+      const user = this._userRepository.create(data);
+      return await this._userRepository.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException({
+          statusCode: 409,
+          message: `An account, ${data.email} already exists `,
+        });
+      }
+      throw e;
+    }
+  }
+}

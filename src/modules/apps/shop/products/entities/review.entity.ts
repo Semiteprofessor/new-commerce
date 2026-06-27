3f47c0ed07e59f19b59b33c8eacad01fcc363af3
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-27 21:57:37 +0100

    Review Entity

--- src/modules/apps/shop/products/entities/review.entity.ts
+++ src/modules/apps/shop/products/entities/review.entity.ts
@@ -1,7 +1,27 @@
 import { BaseEntity } from "src/db/entity/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity, Index, ManyToOne } from "typeorm";
+import { Product } from "./product.entity";
+import { User } from "src/modules/core/users/entities/user.entity";
 
-@Entity("reviews")
+@Entity('reviews')
 export class Review extends BaseEntity {
-    
+  @Column({ type: 'varchar', nullable: true })
+  comment: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  type: string;
+
+  @Column({ type: 'int', nullable: false })
+  rating: number;
+
+  @Column({ type: 'json', nullable: true })
+  issues: string[];
+
+  @Index()
+  @ManyToOne(() => Product, (product) => product.reviews)
+  product: Product;
+
+  @Index()
+  @ManyToOne(() => User, (user) => user.reviews, { cascade: true })
+  user: User;
 }

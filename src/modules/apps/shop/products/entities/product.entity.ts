7a7ee6aa6e016173a8227e74396c047cf6310cb1
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-27 21:53:10 +0100

    Product Entity

--- src/modules/apps/shop/products/entities/product.entity.ts
+++ src/modules/apps/shop/products/entities/product.entity.ts
@@ -1,7 +1,25 @@
 import { BaseEntity } from "src/db/entity/base.entity";
-import { Entity } from "typeorm";
+import { User } from "src/modules/core/users/entities/user.entity";
+import { Column, Entity, Index, ManyToOne } from "typeorm";
 
-@Entity("products")
+@Entity('products')
 export class Product extends BaseEntity {
-    
+  @Column({ type: 'text' })
+  productName: string;
+
+  @ManyToOne(() => Category, (category) => category.products, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @Index()
+  category: Category;
+
+  @ManyToOne(() => User, {
+    nullable: false,
+  })
+  merchant: User;
+
+  @Column({ nullable: true })
+  @Index()
+  merchantId: string;
 }

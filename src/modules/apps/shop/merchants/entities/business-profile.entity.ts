62921f92bcf0c54b4fdaa2278aeff0575c412ff6
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-27 17:36:28 +0100

    eslintrc

---
+++ src/modules/apps/shop/merchants/entities/business-profile.entity.ts
@@ -0,0 +1,8 @@
+import { BaseEntity } from 'src/db/entity/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'business_profile' })
+export class BusinessProfile extends BaseEntity {
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+}
--- src/modules/core/users/entities/user.entity.ts
+++ src/modules/core/users/entities/user.entity.ts
@@ -1,10 +1,39 @@
-import { BaseEntity } from "src/db/entity/base.entity";
-import { Entity, In, Index } from "typeorm";
+import { BaseEntity } from 'src/db/entity/base.entity';
+import { Column, Entity, In, Index, OneToMany } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
-@Entity("users")
-@Index(['email', 'role'], {unique: true})
-@Index(["phone", "phone2"])
-@Index(["role"])
+@Entity('users')
+@Index(['email', 'role'], { unique: true })
+@Index(['phone', 'phone2'])
+@Index(['role'])
 export class User extends BaseEntity {
-    
+  @Column({ type: 'varchar', nullable: true, unique: true, length: 255 })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  firstName?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  middleName?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  lastName?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  email?: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: true })
+  googleId?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    secret: false,
+    default: 'PLACEHOLDER_PASSWORD',
+    nullable: true,
+  })
+  @Exclude({ toPlainOnly: true })
+  password: string;
+
+  @OneToMany(() => Busine)
 }

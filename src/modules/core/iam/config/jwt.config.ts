98afabc9a4bc4e5744766d67d4395a96f98b727e
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-27 17:05:05 +0100

    JWT Config

---
+++ src/modules/core/iam/config/jwt.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('jwt', () => {
+  return {
+    secret: process.env.JWT_SECRET,
+    accessTokenTtl: process.env.JWT_ACCESS_TOKEN_TTL ?? '3600',
+    refreshTokenTtl: process.env.JWT_REFRESH_TOKEN_TTL ?? '86400',
+  };
+});

268dfbc9c75b43d96bae54014e89c766269ddf54
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-29 18:58:06 +0100

    Commit

--- src/modules/apps/shop/wishlist/services/wishlist.service.ts
+++ src/modules/apps/shop/wishlist/services/wishlist.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { WishlistRepository } from '../repositories/wishlist.repository';
 import { UserRepository } from 'src/modules/core/users/repositories/user.repository';
 import { ProductRepository } from '../../products/repositories/product.repository';
+import { Wishlist } from '../entities/wishlist.entity';
 
 @Injectable()
 export class WishlistService {
@@ -10,4 +11,44 @@ export class WishlistService {
     private readonly userRepository: UserRepository,
     private readonly productRepository: ProductRepository,
   ) {}
+
+  async addToWishlist(productId: string, userId: string): Promise<Wishlist> {
+    if (!productId) {
+      throw new BadRequestException('Product ID is required.');
+    }
+
+    const user = await this.userRepository.findOne({ id: userId });
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    const product = await this.productRepository.findOne({ id: productId });
+    if (!product) {
+      throw new NotFoundException('Product not found.');
+    }
+
+    let wishlist = await this.wishlistRepository.findOne(
+      { user: { id: userId } },
+      { relations: ['products'] },
+    );
+
+    if (wishlist && wishlist.products.some((p) => p.id === productId)) {
+      throw new ConflictException('Product already in wishlist');
+    }
+
+    if (!wishlist) {
+      wishlist = await this.wishlistRepository.create({
+        user,
+        products: [product],
+      });
+    } else {
+      wishlist.products.push(product);
+      await this.wishlistRepository.updateWishlistProducts(
+        wishlist.id,
+        wishlist.products,
+      );
+    }
+
+    return wishlist;
+  }
 }

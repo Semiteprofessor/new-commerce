b5c471e4a92eae73a30618084c1b8db9caba9f8f
Taiwo Olapade <semiteprofessor@gmail.com>
2026-06-29 20:08:09 +0100

    Commit

--- src/modules/apps/shop/wishlist/controllers/wishlist.controller.ts
+++ src/modules/apps/shop/wishlist/controllers/wishlist.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { WishlistService } from '../services/wishlist.service';
 import { Wishlist } from '../entities/wishlist.entity';
 import { ActiveUser } from 'src/modules/core/iam/decorators/active-user.decorator';
 import { ActorUser } from 'src/modules/common/types/user.types';
+import { PaginatedRecordsDto, QueryParamsDto } from 'src/modules/common/dtos/pagination.dto';
+import { Product } from '../../products/entities/product.entity';
 
 @Controller('v1/shoppers/wishlist')
 export class WishlistController {
@@ -17,5 +19,22 @@ export class WishlistController {
     console.log('Product ID', body.productId);
 
     return this.wishlistService.addToWishlist(body.productId, actor.id);
+  }
+
+  @Get('all')
+  async getWishlist(
+    @ActiveUser() actor: ActorUser,
+    @Query() query: QueryParamsDto,
+  ): Promise<PaginatedRecordsDto<Product>> {
+    return this.wishlistService.getWishlist(actor.id, query);
+  }
+
+  @Delete(':productId')
+  async removeFromWishlist(
+    @ActiveUser() actor: ActorUser,
+    @Param('productId') productId: string,
+  ): Promise<{ message: string }> {
+    await this.wishlistService.removeFromWishlist(actor.id, productId);
+    return { message: 'Product removed from wishlist successfully' };
   }
 }

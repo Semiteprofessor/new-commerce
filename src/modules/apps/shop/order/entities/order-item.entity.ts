e2f5a375f6d9d07cbf23b1591529292fb2d97867
Taiwo Olapade <semiteprofessor@gmail.com>
2026-07-12 16:18:31 +0100

    Correction

--- src/modules/apps/shop/order/entities/order-item.entity.ts
+++ src/modules/apps/shop/order/entities/order-item.entity.ts
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import { BaseEntity } from '../../../../../db/entity/base.entity';
 import { Column, Entity, Index, ManyToOne, OneToOne } from 'typeorm';
 import { Order } from './order.entity';
 import { Product } from '../../products/entities/product.entity';
 import { User } from '../../../../../modules/core/users/entities/user.entity';
-=======
-import { BaseEntity } from 'src/db/entity/base.entity';
-import { Column, Entity, Index, ManyToOne, OneToOne } from 'typeorm';
-import { Order } from './order.entity';
-import { Product } from '../../products/entities/product.entity';
-import { User } from 'src/modules/core/users/entities/user.entity';
->>>>>>> cbb35b8b55f480354592d7ff588611c60bd980a2
 import { OrderStatus } from '../enums/order.enum';
 import { ReturnRequest } from './return-request.entity';
 

abb6eb911d2eb74776f09bff6d26e2af22597bd6
Taiwo Olapade <semiteprofessor@gmail.com>
2026-07-12 18:24:36 +0100

    Commit

--- src/modules/config/database.config.ts
+++ src/modules/config/database.config.ts
@@ -1,18 +1,32 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const isDevelopment = process.env.NODE_ENV === 'local';
-const DatabaseConfig = () => {
-  return {
-    type: 'postgres',
-    url: process.env.DB_URL,
-    synchronize: false, // set to true only in dev mode
-    entities: ['dist/modules/**/*.entity.{ts,js}'],
-    migrations: ['dist/db/migrations/**/*.{ts,js}'],
-    logging: ['error'], //"query" | "schema" | "error" | "warn" | "info" | "log" | "migration"
-    logger: 'advanced-console',
-    ...(isDevelopment ? {} : { ssl: { rejectUnauthorized: false } }),
-  };
-};
 
-export default DatabaseConfig;
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+export default () => ({
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: Number(process.env.TYPEORM_PORT),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+
+  synchronize: false,
+  autoLoadEntities: true,
+
+  entities: isDevelopment ? ['src/**/*.entity.ts'] : ['dist/**/*.entity.js'],
+
+  migrations: isDevelopment
+    ? ['src/db/migrations/**/*.ts']
+    : ['dist/db/migrations/**/*.js'],
+
+  logging: ['error'],
+  logger: 'advanced-console',
+
+  ssl: isDevelopment
+    ? false
+    : {
+        rejectUnauthorized: false,
+      },
+});
